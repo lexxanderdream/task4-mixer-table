489ea3b5d8858115f084b0d2454284664367a01a
Alexander Zhuchkov <LexxanderDream@gmail.com>
2024-02-10 16:30:32 +0300

    fix

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-Тут важно, что в DatSource мы должны хранить не сами Item, а их идентификаторы
+Тут важно, что в DataSource мы должны хранить не сами элементы, а их идентификаторы
 Это описано в официальном гайде от Apple
 https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/updating_collection_views_using_diffable_data_sources
--- mixer-table/ViewController.swift
+++ mixer-table/ViewController.swift
@@ -32,7 +32,7 @@ class ViewController: UIViewController {
     }()
     
     // !!!!!!!!!!!!!!
-    // Тут важно, что в DatSource мы должны хранить не сами Item, а их идентификаторы
+    // Тут важно, что в DataSource мы должны хранить не сами Item, а их идентификаторы
     // Это описано в официальном гайде от Apple
     // https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/updating_collection_views_using_diffable_data_sources
     //
