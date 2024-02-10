8a02080f0bf22309cc3620587e9fffe99c0d21bd
Alexander Zhuchkov <LexxanderDream@gmail.com>
2024-02-10 16:26:04 +0300

    fix

--- mixer-table/ViewController.swift
+++ mixer-table/ViewController.swift
@@ -33,7 +33,8 @@ class ViewController: UIViewController {
     
     // !!!!!!!!!!!!!!
     // Тут важно, что в DatSource мы должны хранить не сами Item, а их идентификаторы
-    // Это описано в официальном гайде от Apple https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/updating_collection_views_using_diffable_data_sources
+    // Это описано в официальном гайде от Apple
+    // https://developer.apple.com/documentation/uikit/views_and_controls/collection_views/updating_collection_views_using_diffable_data_sources
     //
     // !!!!!!!!!!!!!!
     lazy var dataSource = UITableViewDiffableDataSource<String, Item.ID>(tableView: tableView) { [weak self] tableView, indexPath, itemIdentifier in
